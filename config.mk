e22c577420ecb294cf0d3cec247ae698a3e10e2c
jro1979oliver <jroliveira.oliveira301@gmail.com>
2024-03-01 20:32:17 +0100

    motorola: Add extras for moto devices
    Nothing to see here
    
    Change-Id: I721c91a00d049ca5d17b9b017c277948347f1fdf

---
+++ config.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2024 The Pixel Experience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/motorola-extra/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/motorola-extra/overlay
